7d293f323138f842a7417999431d755cbe0fe48d
Crystal C <crystal.contr@gmail.com>
2018-12-03 05:04:29 -0600

    Removed FourSquare API and CSS

--- public/main.js
+++ public/main.js
@@ -1,47 +1,16 @@
-// Foursqaure API Info
-const clientID = 'BKEAKNSWEY3HG45E0YE3FXFC0C0H3CNY1V4URMRJW0M1PLEK';
-const clientSecret = 'ED3I2U5UNBVGKAD3GDQ2GEUUP3G4AP1SHHPQ5ZGCUOYQVLIM';
-const url = 'https://api.foursquare.com/v2/venues/explore?near=';
-
 // APIXU Info
-const apiKey = 'aa16bf1b5b38405baf772045183011';
-const forecastUrl = 'https://api.apixu.com/v1/forecast.json?key=';
-
-
+const apiKey = '&APPID=6f2c26f2eea8cdaefefd94729d081acf';
+const forecastUrl = 'http://api.openweathermap.org/data/2.5/';
 
 // Page Elements
 const $input = $('#city');
 const $submit = $('#button');
 const $destination = $('#destination');
 const $container = $('.container');
-const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
-const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4")];
+const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4"), $("#weather5")];
 const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 // AJAX functions
-async function getVenues() {
-    const city = $input.val();
-    const urlToFetch = url + city + '?limit=10&client_id=' +
-    clientID + '&client_secret=' + clientSecret + '&v=20181130';
-
-    try {
-        let response = await fetch(urlToFetch);
-        if (response.ok) {
-            console.log(response);
-            let jsonResponse = await response.json();
-            console.log(jsonResponse);
-            let venues = jsonResponse.response.groups[0].items.map(item => item.venue);
-            console.log(venues);
-            return venues;
-        }
-        else {
-            throw new Error('Request failed!');
-        }
-    } catch (error) {
-        console.log(error);
-    }
-};
-
 async function getForecast() {
     const urlToFetch = forecastUrl + apiKey + '&q=' + $input.val() + '&days=4&hours=11';
 
@@ -49,7 +18,7 @@ async function getForecast() {
         let response = await fetch(urlToFetch);
         if (response.ok) {
             let jsonResponse = await response.json();
-            // console.log(jsonResponse);
+            console.log(jsonResponse);
             let days = jsonResponse.forecast.forecastday;
             return days;
         }
@@ -59,34 +28,6 @@ async function getForecast() {
 }
 
 // Render functions
-function renderVenues(venues) {
-    $venueDivs.forEach(($venue, index) => {
-        // console.log('This is the venue ' + $venue);
-        // console.log('This is the index ' + index);
-        const venue = venues[index];
-        const venueIcon = venue.categories[0].icon;
-        const venueImgSrc = venueIcon.prefix + 'bg_64' + venueIcon.suffix;
-        let venueContent = createVenueHTML(venue.name, venue.location, venueImgSrc);
-        $venue.append(venueContent);
-        });
-        $destination.append(`<h2>${venues[0].location.city}</h2>`);
-
-
-    //     let venueContent = 
-    //         '<h2>' + venues[index].name + '</h2>' +
-    //         '<img class="venueimage" src="' + imgPrefix + venues[index].photos.groups[0].items[0].suffix +
-    //         '"/>' + 
-    //         '<h3>Address:</h3>' + 
-    //         '<p>' + venues[index].location.address + '</p>' +
-    //         '<p>' + venues[index].location.city + '</p>' +
-    //         '<p>' + venues[index].location.country + '</p>' +
-    //         // '<h3>Category:</h3>' + 
-    //         // '<p>' + venues[index].categories[0].name + '</p>';
-    //     $venue.append(venueContent);
-    // });
-    // $destination.append('<h2>' + venues[0].location.city + '</h2>');
-}
-
 function renderForecast(days) {
     $weatherDivs.forEach(($day, index) => {
       let weatherContent =
@@ -104,7 +45,6 @@ function executeSearch() {
     $weatherDivs.forEach(day => day.empty());
     $destination.empty();
     $container.css("visibility", "visible");
-    getVenues().then(venues => renderVenues(venues));
     getForecast().then(forecast => renderForecast(forecast));
     return false;
 }
