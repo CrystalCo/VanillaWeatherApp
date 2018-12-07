59c3a7d86513698f341f756deccdb19ee55a2ef9
Crystal C <crystal.contr@gmail.com>
2018-12-07 01:10:41 -0600

    Added Current WeatherAPI function, HTML, & CSS.

--- public/main.js
+++ public/main.js
@@ -8,12 +8,29 @@ const $input = $('#city');
 const $submit = $('#button');
 const $destination = $('#destination');
 const $container = $('.container');
+const $currentWeather = $('#weatherNow') ;
 const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4"), $("#weather5")];
 const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 // AJAX functions
+async function getCurrentForecast() {
+    const urlToFetch = forecastUrl + 'weather?q=' + $input.val() + '&units=imperial' + apiKey;
+
+    try {
+        let response = await fetch(urlToFetch);
+        if (response.ok) {
+            let jsonResponse = await response.json();
+            console.log(jsonResponse);
+            let currentDay = jsonResponse;
+            return currentDay;
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
+
 async function get5DayForecast() {
-    const urlToFetch = forecastUrl + 'forecast?q=' + $input.val() + apiKey;
+    const urlToFetch = forecastUrl + 'forecast?q=' + $input.val() + '&units=imperial' + apiKey;
 
     try {
         let response = await fetch(urlToFetch);
@@ -29,22 +46,31 @@ async function get5DayForecast() {
 }
 
 // Render functions
-function kelvinToFahrenheit(kelvin) {
-    fahren = 9 / 5 * (kelvin - 273) + 32;
-    return Math.round(fahren * 10) / 10;
+function renderCurrentForecast(currentDay) {    
+    const currentTemp = Math.round(currentDay.main.temp_max);
+    const today = weekDays[(new Date(currentDay.dt * 1000).getDay())-1];
+    console.log("New Date: " + new Date(currentDay.dt * 1000).getDay());
+    let weatherContent =
+    '<h2>' + currentTemp + ' F&deg; </h2>' +
+    '<img src="' + weatherIconUrl + currentDay.weather[0].icon +
+    '.png" class="weathericon" />' +
+    '<h3>' + currentDay.weather[0].description + '</h3>' +
+    '<h2>' + today + '</h2>';
+    
+    $currentWeather.append(weatherContent);
 }
 
-function renderForecast(days) {
+function render5DayForecast(days) {
     $weatherDivs.forEach(($day, index) => {
-      const maxTempInF = kelvinToFahrenheit(days[index].main.temp_max);
-      const minTempInF = kelvinToFahrenheit(days[index].main.temp_min);
+      const maxTempInF = Math.round(days[index].main.temp_max);
+      const minTempInF = Math.round(days[index].main.temp_min);
       let weatherContent =
-        '<h2> High: ' + maxTempInF + ' F </h2>' +
-        '<h2> Low: ' + minTempInF + ' F </h2>' +
+        '<h2> High: ' + maxTempInF + ' F&deg; </h2>' +
+        '<h2> Low: ' + minTempInF + ' F&deg; </h2>' +
         '<img src="' + weatherIconUrl + days[index].weather[0].icon +
             '.png" class="weathericon" />' +
         '<h3>' + days[index].weather[0].description + '</h3>' +
-        '<h2>' + weekDays[(new Date(days[index].dt)).getDay()] + '</h2>';
+        '<h2>' + weekDays[(new Date(days[index].dt * 1000).getDay())-1] + '</h2>';
       $day.append(weatherContent);
     });
 }
@@ -53,7 +79,8 @@ function executeSearch() {
     $weatherDivs.forEach(day => day.empty());
     $destination.empty();
     $container.css("visibility", "visible");
-    get5DayForecast().then(forecast => renderForecast(forecast));
+    getCurrentForecast().then(forecast => renderCurrentForecast(forecast));
+    get5DayForecast().then(forecast => render5DayForecast(forecast));
     return false;
 }
 
